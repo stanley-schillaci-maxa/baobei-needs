1cafffd70075d59bcd76e0a7bc32a4a9b18fa8ea
Adrien Turiot <aturiot@pyx4.com>
2020-08-29 03:27:47 -0400

    Use event subscriber in movement system

--- src/movement.rs
+++ src/movement.rs
@@ -1,7 +1,11 @@
 //! System managing changes in entity movement.
 
-use crate::{controllers::Direction, physics::Velocity};
-use crossbeam_channel::Receiver;
+use crate::{
+    controllers::Direction,
+    events::{EventsExt, Subscriber},
+    physics::Velocity,
+};
+
 use legion::{prelude::*, storage::Tag};
 
 /// Event appearing when a movement happen.
@@ -14,30 +18,28 @@ pub struct MovementSystem<TTag: Tag> {
     /// Speed used to update the velocity
     speed: f32,
     /// Subscription to movement events
-    events: Receiver<Event>,
+    events: Subscriber<MovementEvent>,
 }
 
 impl<TTag: Tag> MovementSystem<TTag> {
     /// Creates a `MovementSystem` for moving the tagged entity.
     pub fn new(world: &mut World, tag: TTag, speed: f32) -> Self {
-        let (sender, receiver) = crossbeam_channel::unbounded();
-        world.subscribe(sender, component::<MovementEvent>());
-
         Self {
             tag,
             speed,
-            events: receiver,
+            events: world.subscribe_events::<MovementEvent>(),
         }
     }
 
     /// Updates the velocity of the tagged entity
     pub fn update(&self, world: &mut World) {
-        if let Some(Event::EntityInserted(entity, _)) = self.events.try_iter().last() {
-            let new_velocity = match world.get_component::<MovementEvent>(entity) {
-                Some(movement) => self.speed * movement.0,
-                None => Velocity::new(0.0, 0.0),
-            };
+        let new_velocity = self
+            .events
+            .iter_events(world)
+            .last()
+            .map(|movement| self.speed * movement.0);
 
+        if let Some(new_velocity) = new_velocity {
             Write::<Velocity>::query()
                 .filter(tag_value(&self.tag))
                 .iter_mut(world)
