4020d72eaaa8dcd46c66e0e6df76109de88c14af
Adrien Turiot <aturiot@pyx4.com>
2020-08-29 03:27:12 -0400

    Add event subscriber to the world

--- src/events.rs
+++ src/events.rs
@@ -1,7 +1,8 @@
 //! Types and helpers for interacting with appearing events.
 
-use legion::{prelude::*, storage::Component};
-use std::iter::once;
+use crossbeam_channel::Receiver;
+use legion::{borrow::Ref, event::Event as LegionEvent, prelude::*, storage::Component};
+use std::{iter::once, marker::PhantomData};
 
 /// Static tag to assign event entities with.
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -14,6 +15,9 @@ pub trait EventsExt {
 
     /// Removes all events entities from the world.
     fn clear_events(&mut self);
+
+    /// Returns a subscriber for events of the given type
+    fn subscribe_events<TEvent: Component>(&mut self) -> Subscriber<TEvent>;
 }
 
 impl EventsExt for World {
@@ -30,5 +34,38 @@ impl EventsExt for World {
         for entity in event_entities {
             self.delete(entity);
         }
+    }
+
+    fn subscribe_events<TEvent: Component>(&mut self) -> Subscriber<TEvent> {
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        self.subscribe(sender, component::<TEvent>());
+
+        Subscriber {
+            receiver,
+            phantom: PhantomData,
+        }
+    }
+}
+
+/// Subscriber of events of type `TEvent` that are send into the world
+pub struct Subscriber<TEvent: Component> {
+    /// Receiver of legion event
+    receiver: Receiver<LegionEvent>,
+    /// Marker for the above `TEvent` generic type
+    phantom: PhantomData<TEvent>,
+}
+
+impl<TEvent: Component> Subscriber<TEvent> {
+    /// Returns events that have been inserted to the world.
+    pub fn iter_events<'world>(
+        &'world self,
+        world: &'world World,
+    ) -> impl Iterator<Item = Ref<'world, TEvent>> {
+        self.receiver
+            .try_iter()
+            .filter_map(move |event| match event {
+                LegionEvent::EntityInserted(entity, _) => world.get_component::<TEvent>(entity),
+                _ => None,
+            })
     }
 }
