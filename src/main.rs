d6e85c780cdfd0fdd49f09364924469cb947c966
Adrien Turiot <aturiot@pyx4.com>
2020-11-14 02:36:15 -0500

    refactor: Move entities into the scene file

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ mod scenes;
 
 use bevy::{input::system::exit_on_esc_system, prelude::*};
 
-use collisions::{BoxCollider, CollisionPlugin, Position};
+use collisions::{CollisionPlugin, Position};
 use constants::{SPEED, WINDOW_HEIGHT, WINDOW_WIDTH};
 use controllers::{ControllerPlugin, DirectionEvent};
 use scenes::SceneLoaderPlugin;
@@ -32,10 +32,12 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .register_component::<Didi>()
+        .register_component::<Furniture>()
+        .register_component::<Baobei>()
         .add_plugin(ControllerPlugin)
         .add_plugin(CollisionPlugin)
         .add_plugin(SceneLoaderPlugin)
-        .add_startup_system(setup_entities.system())
+        .add_startup_system(setup_camera.system())
         .add_system_to_stage(stage::FIRST, exit_on_esc_system.system())
         .add_system_to_stage(stage::UPDATE, movement_system.system())
         .add_system_to_stage(stage::POST_UPDATE, drawing_system.system())
@@ -46,103 +48,19 @@ fn main() {
 #[derive(Properties, Default)]
 struct Didi;
 /// The baobei to take care of
+#[derive(Properties, Default)]
 struct Baobei;
 /// Furniture containing items
+#[derive(Properties, Default)]
 struct Furniture;
 
-/// Spawn the camera, Didi and Baobei.
-fn setup_entities(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let color_handle = materials.add(Color::rgba(0.3, 1.0, 0.3, 0.3).into());
-
-    let box_collider_sprite = |size: Vec2| SpriteComponents {
-        material: color_handle.clone(),
-        sprite: Sprite::new(size),
-        ..SpriteComponents::default()
-    };
-
+/// Spawn the camera.
+fn setup_camera(mut commands: Commands) {
     let mut camera_2d = Camera2dComponents::default();
     camera_2d.transform.translation +=
         Vec3::new(WINDOW_WIDTH as f32 / 2.0, WINDOW_HEIGHT as f32 / 2.0, 0.0);
 
     commands.spawn(camera_2d);
-
-    let couch_position = Position((1000.0, 150.0, 0.0).into());
-    let couch_size = Vec2::new(100.0, 100.0);
-
-    commands
-        .spawn((Furniture, couch_position, BoxCollider { size: couch_size }))
-        .with_bundle(SpriteComponents {
-            material: materials.add(asset_server.load("couch.png").into()),
-            transform: Transform::from_scale(Vec3::new(0.4, 0.4, 0.0)),
-            ..SpriteComponents::default()
-        })
-        .spawn((Furniture, couch_position))
-        .with_bundle(box_collider_sprite(couch_size));
-
-    let fridge_position = Position((720.0, 540.0, 0.0).into());
-    let fridge_size = Vec2::new(100.0, 100.0);
-
-    commands
-        .spawn((
-            Furniture,
-            fridge_position,
-            BoxCollider { size: fridge_size },
-        ))
-        .with_bundle(SpriteComponents {
-            material: materials.add(asset_server.load("fridge.png").into()),
-            transform: Transform::from_scale(Vec3::new(0.35, 0.35, 0.0)),
-            ..SpriteComponents::default()
-        })
-        .spawn((Furniture, fridge_position))
-        .with_bundle(box_collider_sprite(fridge_size));
-
-    let kitchen_position = Position((300.0, 540.0, 0.0).into());
-    let kitchen_size = Vec2::new(100.0, 100.0);
-
-    commands
-        .spawn((
-            Furniture,
-            kitchen_position,
-            BoxCollider { size: kitchen_size },
-        ))
-        .with_bundle(SpriteComponents {
-            material: materials.add(asset_server.load("kitchen.png").into()),
-            transform: Transform::from_scale(Vec3::new(0.5, 0.5, 0.0)),
-            ..SpriteComponents::default()
-        })
-        .spawn((Furniture, kitchen_position))
-        .with_bundle(box_collider_sprite(kitchen_size));
-
-    let sink_position = Position((1050.0, 450.0, 0.0).into());
-    let sink_size = Vec2::new(100.0, 100.0);
-
-    commands
-        .spawn((Furniture, sink_position, BoxCollider { size: sink_size }))
-        .with_bundle(SpriteComponents {
-            material: materials.add(asset_server.load("sink.png").into()),
-            transform: Transform::from_scale(Vec3::new(0.3, 0.3, 0.0)),
-            ..SpriteComponents::default()
-        })
-        .spawn((Furniture, sink_position))
-        .with_bundle(box_collider_sprite(sink_size));
-
-    let size = Vec2::new(100.0, 20.0);
-
-    let baobei_position = Position((1050.00, 250.0, 0.0).into());
-
-    commands
-        .spawn((Baobei, baobei_position, BoxCollider { size }))
-        .with_bundle(SpriteComponents {
-            material: materials.add(asset_server.load("baobei.png").into()),
-            transform: Transform::from_scale(Vec3::new(0.3, 0.3, 0.0)),
-            ..SpriteComponents::default()
-        })
-        .spawn((Baobei, baobei_position))
-        .with_bundle(box_collider_sprite(size));
 }
 
 /// Moves Didi toward the direction sent by controllers.
