ec6fcac2650095de33d3199930629b525c3da5a9
Adrien Turiot <aturiot@pyx4.com>
2021-01-07 00:01:15 -0500

    feat: Spawn a sprite for the carried item

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -25,7 +25,7 @@ impl Plugin for GameplayPlugin {
             .add_startup_system(setup_camera.system())
             .add_startup_system(spawn_didi.system())
             .add_startup_system(spawn_colliders.system())
-            .add_resource(PickAndDropCooldown(Cooldown::from_seconds(0.5)))
+            .add_resource(PickAndDropCooldown(Cooldown::from_seconds(0.2)))
             .on_state_update(STAGE, GameState::InGame, back_to_menu_system.system())
             .on_state_update(STAGE, GameState::InGame, movement_system.system())
             .on_state_update(STAGE, GameState::InGame, pick_or_drop_system.system())
@@ -56,6 +56,12 @@ struct GameData {
 struct GameplayMaterials {
     /// Transparent color
     didi_sprite: Handle<ColorMaterial>,
+    /// Sprite for the ice cream item
+    ice_cream_sprite: Handle<ColorMaterial>,
+    /// Sprite for the water glass item
+    water_glass_sprite: Handle<ColorMaterial>,
+    /// Sprite for the chips item
+    chips_sprite: Handle<ColorMaterial>,
 }
 
 impl FromResources for GameplayMaterials {
@@ -64,6 +70,20 @@ impl FromResources for GameplayMaterials {
         let asset_server = resources.get_mut::<AssetServer>().unwrap();
         Self {
             didi_sprite: materials.add(asset_server.load("didi.png").into()),
+            ice_cream_sprite: materials.add(asset_server.load("items/ice_cream.png").into()),
+            water_glass_sprite: materials.add(asset_server.load("items/water_glass.png").into()),
+            chips_sprite: materials.add(asset_server.load("items/chips.png").into()),
+        }
+    }
+}
+
+impl GameplayMaterials {
+    /// Returns the sprite handle for the given item
+    fn item_sprite_for(&self, item: Item) -> Handle<ColorMaterial> {
+        match item {
+            Item::IceCream => self.ice_cream_sprite.clone(),
+            Item::WaterGlass => self.water_glass_sprite.clone(),
+            Item::Chips => self.chips_sprite.clone(),
         }
     }
 }
@@ -101,13 +121,13 @@ fn spawn_didi(commands: &mut Commands, materials: Res<GameplayMaterials>) {
 /// Spawn a temporary colliders for testing.
 fn spawn_colliders(commands: &mut Commands) {
     commands.spawn((
-        Position(Vec3::new(900.0, 260.0, 0.0)),
+        Position(Vec3::new(900.0, 600.0, 0.0)),
         BoxCollider::new(100.0, 100.0),
         TriggerArea::new(150.0, 150.0),
         ItemProducer(Item::WaterGlass),
     ));
     commands.spawn((
-        Position(Vec3::new(300.0, 260.0, 0.0)),
+        Position(Vec3::new(300.0, 600.0, 0.0)),
         TriggerArea::new(200.0, 200.0),
         BoxCollider::new(100.0, 100.0),
         ItemProducer(Item::IceCream),
@@ -160,6 +180,9 @@ pub enum Item {
 /// Component on entities carrying an item.
 pub struct Carrying(pub Item);
 
+/// Component on entities that is a carried item.
+pub struct CarriedItem;
+
 /// Component on entities that can produce the item.
 pub struct ItemProducer(pub Item);
 
@@ -176,7 +199,7 @@ enum ActionEvent {
     Keep(Item),
 }
 
-/// Cooldown of the action of picking or droping items.
+/// Cooldown of the action of picking or dropping items.
 struct PickAndDropCooldown(Cooldown);
 
 /// Pick or drop an item in an item producer.
@@ -218,6 +241,8 @@ fn handle_actions_system(
     mut action_event_reader: Local<EventReader<ActionEvent>>,
     action_events: Res<Events<ActionEvent>>,
     game_data: Res<GameData>,
+    materials: Res<GameplayMaterials>,
+    carried_items: Query<Entity, With<CarriedItem>>,
 ) {
     let didi = game_data.didi_entity;
 
@@ -226,10 +251,26 @@ fn handle_actions_system(
             ActionEvent::Drop(item) => {
                 info!("Drop item {:?}", item);
                 commands.remove_one::<Carrying>(didi);
+
+                for item_in_hand in carried_items.iter() {
+                    commands.despawn(item_in_hand);
+                }
             }
             ActionEvent::Pick(item) => {
                 info!("Pick item {:?}", item);
                 commands.insert_one(didi, Carrying(*item));
+
+                let item_in_hand = commands
+                    .spawn(SpriteBundle {
+                        material: materials.item_sprite_for(*item),
+                        transform: Transform::from_translation(Vec3::new(-170.0, -10.0, 0.0)),
+                        ..SpriteBundle::default()
+                    })
+                    .with(CarriedItem)
+                    .current_entity()
+                    .unwrap();
+
+                commands.push_children(didi, &[item_in_hand]);
             }
             ActionEvent::Keep(item) => info!("Keep item {:?}", item),
         }
