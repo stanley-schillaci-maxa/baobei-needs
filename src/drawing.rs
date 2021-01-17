896187556977d3361ec339cd1c74987b17461155
Adrien Turiot <aturiot@pyx4.com>
2021-01-16 20:47:01 -0500

    feat: Introduce the window scale managemnt (WIP)

--- src/drawing.rs
+++ src/drawing.rs
@@ -2,7 +2,10 @@
 
 use bevy::prelude::*;
 
-use crate::{collisions::Position, constants::WINDOW_HEIGHT};
+use crate::{
+    collisions::Position,
+    constants::{WINDOW_HEIGHT, WINDOW_WIDTH},
+};
 
 /// Plugin the drawing things on the screen.
 pub struct DrawingPlugin;
@@ -22,14 +25,23 @@ pub struct UIObject;
 const Z_LIMIT: f32 = 1000.0;
 
 /// Updates transform of game objects following their game position.
+///
+/// TODO: Scales the translation and sprite scale with the window size.
+/// This needs the scale as a component and center positions in the middle
+/// of the window
 fn update_game_object_position_system(
+    // windows: Res<Windows>,
     mut game_objects: Query<(&Position, &mut Transform), Without<(Parent, UIObject)>>,
 ) {
+    // TODO: scale depending on window size
+    // let window = windows.get_primary().unwrap();
+    // dbg!(window.width(), window.height());
+    // window_size_ratio(window);
+
     for (position, mut transform) in game_objects.iter_mut() {
         transform.translation = position.0;
 
-        // Scale the z index between 0 and 1000 depending on the y index.
-        // 0 = background, 1000 = foreground
+        // Scale the z index depending on the y index.
         transform.translation.z = Z_LIMIT - position.0.y * Z_LIMIT / WINDOW_HEIGHT;
 
         // Move up the entities in the air.
@@ -45,4 +57,13 @@ fn update_ui_objects_position_system(
         transform.translation = position.0;
         transform.translation.z = Z_LIMIT - 1.0;
     }
+}
+
+/// Returns the ratio between the current size and the initial one.
+fn window_size_ratio(window: Window) -> Vec3 {
+    Vec3::new(
+        window.width() / WINDOW_WIDTH,
+        window.height() / WINDOW_HEIGHT,
+        1.0,
+    )
 }
